8340a06003477ceb080e001c3fd13bce35149b08
Julie P <paupertjulie@gmail.com>
2020-06-17 15:32:40 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>VueJS Calculator</h1>
+    <Calculator />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Calculator
   }
 }
 </script>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Merriweather:wght@300&family=Montserrat:wght@200&family=Source+Serif+Pro&display=swap');
+
+body {
+  background: #FAACA8;
+  background-image:
+    radial-gradient(50% 30% ellipse at center top, #FAACA8 0%, rgba(0,0,0,0) 100%),
+    radial-gradient(60% 50% ellipse at center bottom, #DDD6F3 0%, #FAACA8 100%);
+  /* background-color: #FAACA8;
+background-image: linear-gradient(19deg, #FAACA8 0%, #DDD6F3 50%, #ffffff 100%); */
+  background-attachment: fixed;
+  color: #6cacc5;
+}
+
+h1 {
+  color: #eee;
+  margin-top: -10px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', sans-serif; 
+  /* font-family: 'Merriweather', serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="calculator">
+    <div class="display">{{ current || '0' }}</div>
+    <div @click="clear" class="button">C</div>
+    <div @click="sign" class="button">+/-</div>
+    <div @click="percent" class="button">%</div>
+    <div @click="divide" class="button operator">÷</div>
+    <div @click="append('7')" class="button">7</div>
+    <div @click="append('8')" class="button">8</div>
+    <div @click="append('9')" class="button">9</div>
+    <div @click="times" class="button operator">x</div>
+    <div @click="append('4')" class="button">4</div>
+    <div @click="append('5')" class="button">5</div>
+    <div @click="append('6')" class="button">6</div>
+    <div @click="minus" class="button operator">-</div>
+    <div @click="append('1')" class="button">1</div>
+    <div @click="append('2')" class="button">2</div>
+    <div @click="append('3')" class="button">3</div>
+    <div @click="add" class="button operator">+</div>
+    <div @click="append('0')" class="button zero">0</div>
+    <div @click="dot" class="button">.</div>
+    <div @click="equal" class="button operator">=</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      previous: null,
+      current: '', //valeur display == empty string
+      operator: null,
+      operatorClicked: false
+    }
+  },
+  methods: {
+    clear() {
+      this.current = ''; //onclick sur clear, efface la valeur display et retour à 0
+    },
+    sign() { //Ajout de + ou - devant la valeur display 
+      this.current = this.current.charAt(0) === '-' ?
+        this.current.slice(1) : `-${this.current}`; //Si - devant valeur display, on l'enlève, si elle n'est pas présente on l'ajoute
+    },
+    percent() { //Transforme la current value en float et la divise par 100
+      this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) { //Concatenate le nombre cliqué avec la current value
+      if (this.operatorClicked) { //Si on clique sur une opération, on clean le state current (empty string) au lieu d'append
+        this.current = ''; 
+        this.operatorClicked = false;
+      }
+      this.current = `${this.current}${number}`;
+    },
+    dot() { 
+      if (this.current.indexOf('.') === -1) { //Si il n'y a pas de point, on rajoute
+        this.append('.');
+      }
+    },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+    },
+    times() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious(); 
+    },
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    equal() { 
+      this.current = `${this.operator(
+        parseFloat(this.previous),
+        parseFloat(this.current)
+      )}`;
+      this.previous = null;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+  .calculator {
+    width: 400px;
+    margin: 0 auto;
+    font-size: 40px;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: minmax(50px, auto);
+  }
+
+  .display {
+    grid-column: 1 / 5;
+    color: #6cacc5;
+    border-radius: 20px;
+    width: 95%;
+    margin: 0 auto;
+  }
+
+  .zero {
+    grid-column: 1 / 3;
+  }
+
+  .button {
+  border: 0;
+  border-radius: 20px;
+  background: #DDD6F3;
+  color: #6cacc5;
+  cursor: pointer;
+  float: left;
+  font: inherit;
+  margin: 0.25em;
+  width: 2em;
+  height: 2em;
+  transition: all 0.5s;
+  }
+  
+  .button:hover {
+    background: #6cacc5;
+    color: #fff;
+    
+  }
+  
+  .button:focus {
+    outline: 0;
+  }
+    
+  .button::after {
+    animation: zoom 1s;
+    animation-iteration-count: 1;
+    animation-fill-mode: both; 
+    content: attr(data-num);
+    cursor: default;
+    font-size: 100px;
+    position: absolute;
+      top: 1.5em;
+      left: 50%;
+    text-align: center;
+    margin-left: -24px;
+    opacity: 0;
+    width: 48px;    
+  }
+
+
+</style>
